67bb4c5900ad95b5b43398187a125a7b1d220a26
Alexey Tolmachev <alexeyttolmachev@yandex.ru>
2026-01-05 17:51:19 +0300

    Completed smart table project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './fonts/ys-display/fonts.css'
 import './style.css'
 
+import { initPagination } from "./components/pagination.js";
+import { initSorting } from "./components/sorting.js";
 import {data as sourceData} from "./data/dataset_1.js";
-
+import { initFiltering } from "./components/filtering.js";
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
-
+import { initSearching } from "./components/searching.js";
 import {initTable} from "./components/table.js";
 // @todo: подключение
 
@@ -18,11 +20,16 @@ const {data, ...indexes} = initData(sourceData);
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
+  const state = processFormData(new FormData(sampleTable.container));
 
-    return {
-        ...state
-    };
+  const rowsPerPage = parseInt(state.rowsPerPage);
+  const page = parseInt(state.page ?? 1);
+
+  return {
+    ...state,
+    rowsPerPage,
+    page,
+  };
 }
 
 /**
@@ -33,20 +40,48 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-
+    result = applySearching(result, state, action);
+    result = applyFiltering(result, state, action);
+    result = applySorting(result, state, action);
+    result = applyPagination(result, state, action);
 
     sampleTable.render(result)
 }
 
-const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: [],
-    after: []
-}, render);
+const sampleTable = initTable(
+  {
+    tableTemplate: "table",
+    rowTemplate: "row",
+    before: ["search", "header", "filter"],
+    after: ["pagination"],
+  },
+  render
+);
 
 // @todo: инициализация
 
+const applyPagination = initPagination(
+  sampleTable.pagination.elements,
+  (el, page, isCurrent) => {
+    const input = el.querySelector("input");
+    const label = el.querySelector("span");
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  }
+);
+
+const applySorting = initSorting([
+  sampleTable.header.elements.sortByDate,
+  sampleTable.header.elements.sortByTotal,
+]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+  searchBySeller: indexes.sellers,
+});
+
+const applySearching = initSearching("search");
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
